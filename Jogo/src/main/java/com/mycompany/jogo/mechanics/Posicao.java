64db7fed9740a52e03296c44b75f31ac787dc855
ViniCampos12 <viniciuspriamo@gmail.com>
2024-09-12 21:17:04 -0300

    Coisas feitas antes já

--- Jogo/src/main/java/com/mycompany/jogo/mechanics/Posicao.java
+++ Jogo/src/main/java/com/mycompany/jogo/mechanics/Posicao.java
@@ -9,5 +9,22 @@ package com.mycompany.jogo.mechanics;
  * @author Vinicius
  */
 public class Posicao {
+    private int l;
+    private int c;
     
+    public Posicao(int l, int c)
+    {
+        this.l = l;
+        this.c = c;
+    }
+    
+    public int getL()
+    {
+        return this.l;
+    }
+    
+    public int getC()
+    {
+        return this.c;
+    }
 }
--- Jogo/src/main/java/com/mycompany/jogo/view/Tabuleiro.java
+++ Jogo/src/main/java/com/mycompany/jogo/view/Tabuleiro.java
@@ -4,10 +4,41 @@
  */
 package com.mycompany.jogo.view;
 
+import com.mycompany.jogo.characters.Personagem;
+
 /**
  *
  * @author Vinicius
  */
 public class Tabuleiro {
+    private Personagem[][] tab;
+    private final int TAM = 11;
+
+    public Tabuleiro() {
+        this.tab = new Personagem[TAM][TAM];
+        this.inicializaTab();
+    }
+    
+    private void inicializaTab(){
+        for(int i = 0; i<TAM;i++){
+            for(int j = 0; j<TAM;j++){
+                this.tab[i][j] = null;
+            }
+        }
+    } 
+    
+    public void imprimeTab(){
+         for(int i = 0; i<TAM;i++){
+            for(int j = 0; j<TAM;j++){
+                if(this.tab[i][j] == null)
+                    System.out.print("Vazio" + " ");
+                else
+                    System.out.print(this.tab[i][j] + " ");
+            }
+             System.out.println();
+        }
+    }
+
+    
     
 }
